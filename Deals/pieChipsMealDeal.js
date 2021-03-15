6ec30dc965d2303dca000890e58396682f806f6a
Muhammad Usman <usmanm4@cardiff.ac.uk>
2021-03-15 02:59:54 +0000

    added Meal Deal Logic

--- Deals/pieChipsMealDeal.js
+++ Deals/pieChipsMealDeal.js
@@ -1,8 +1,25 @@
-
+const numOfChipsInBasket = require("../helperFunctions/chipsCount.js");
+const numOfPieInBasket = require("../helperFunctions/pieCount.js");
+const math = require("mathjs");
+const constants = require("../Constants/constants.js");
 
 
 const pieChipsMealDeal = function(basket) {
+    if (basket.products.length === 0){
+        return new Error("Basket Is Empty Cannot Apply Meal Deal Discount")
+    }
 
+    const numOfPies = numOfPieInBasket(basket);
+    const numOfChips = numOfChipsInBasket(basket);
+    let discountValue = 0;
 
+    const numTotalDiscount = math.min(numOfPies,numOfChips);
 
+    for(let i = 0; i < numTotalDiscount; i++){
+        discountValue += (constants.chipsPrice + constants.piePrice) *  (constants.pieChipsDiscount);
+    }
+    return discountValue;
 } 
+
+module.exports = pieChipsMealDeal;
+
